73d4de858160224f6beea73de378f80c75bb34d2
Daniyal Sohail <daniyalsohail0@gmail.com>
2023-12-12 23:22:15 +0000

    creators page initialized

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing-page";
 import LogIn from "./pages/log-in";
 import SignUp from "./pages/sign-up";
-import ProtectedRoutes from "./routes/ProtectedRoutes";
+import Creator from "./pages/creator";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LogIn />} />
       <Route path="/signup" element={<SignUp />} />
-      <ProtectedRoutes userType={"notLoggedIn"} />
+      <Route path="/creator" element={<Creator />} />
     </Routes>
   );
 }
---
+++ src/pages/creator/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../layout'
+
+const Creator = () => {
+  return (
+    <Layout>
+        Creator
+    </Layout>
+  )
+}
+
+export default Creator
